1f7eea329784df6730390d5c39c4590a040bfc77
alidaka <alidaka@gmail.com>
2016-12-21 17:09:56 -0500

    2016-01 part 2

--- 2016/01/map.rb
+++ 2016/01/map.rb
@@ -1,39 +1,93 @@
 class Map
-  CARDINAL_DIRECTIONS = [:north, :east, :south, :west]
+  CARDINAL_WHEEL = [:north, :east, :south, :west, :north]
+  attr_reader :x, :y
 
-  def calculate(s)
+  def initialize
+    @coordinates = Array.new(500) {Array.new(500, false)}
+
+    @x = 0
+    @y = 0
+
+    @list = Array.new
+    visit(@x, @y)
+  end
+
+  def self.travel(s, stop_on_repeat)
     instructions = s.split(', ')
     cardinal = :north
-    totals = {}
-    CARDINAL_DIRECTIONS.each { |direction| totals[direction] = 0 }
 
+    map = Map.new
     instructions.each do |instruction|
-      turn, distance = parse_instruction(instruction)
-      cardinal = next_cardinal(cardinal, turn)
-      totals[cardinal] += distance
+      cardinal, distance = parse_instruction(instruction, cardinal)
+
+      answer = map.go(cardinal, distance, stop_on_repeat)
+      return from_origin(map.x, map.y) if answer && stop_on_repeat
     end
 
-    roll_up(totals)
+    from_origin(map.x, map.y)
   end
 
-  def roll_up(totals)
-    x = (totals[:east] - totals[:west]).abs
-    y = (totals[:north] - totals[:south]).abs
-    x + y
+  def self.from_origin(x, y)
+      x.abs + y.abs
   end
 
-  def next_cardinal(previous_cardinal, turn)
-    cheat_to_win = CARDINAL_DIRECTIONS + [:north]
-    cheat_to_win.reverse! if turn == :left
+  def self.parse_instruction(instruction, cardinal)
+    turn = instruction[0] == 'R' ? :right : :left
+    cardinal = next_cardinal(cardinal, turn)
 
-    cheat_to_win.each_cons(2) { |a|
+    distance = instruction[1...instruction.length].to_i
+
+    [cardinal, distance]
+  end
+
+  def self.next_cardinal(previous_cardinal, turn)
+    wheel = turn == :right ? CARDINAL_WHEEL : CARDINAL_WHEEL.reverse
+
+    wheel.each_cons(2) { |a|
       return a[1] if a[0] == previous_cardinal
     }
   end
 
-  def parse_instruction(instruction)
-      turn = instruction[0] == 'R' ? :right : :left
-      distance = instruction[1...instruction.length].to_i
-      [turn, distance]
+  def go(cardinal, distance, stop_on_repeat)
+    distance.times {
+      return true if step(cardinal) && stop_on_repeat
+    }
+
+    false
+  end
+
+  def step(cardinal)
+    case cardinal
+    when :north then @y += 1
+    when :south then @y -= 1
+    when :east then @x += 1
+    when :west then @x -= 1
+    end
+
+    visit(@x, @y)
+  end
+
+  def visit(x, y)
+    visited = get(x, y)
+    set(x, y, true)
+    visited
+  end
+
+  def translate(coordinate)
+    image = (2 * coordinate.abs)
+    image = image - 1 if coordinate < 0
+    image
+  end
+
+  def get(x, y)
+    xi = translate(x)
+    yi = translate(y)
+    @coordinates[xi][yi]
+  end
+
+  def set(x, y, value)
+    xi = translate(x)
+    yi = translate(y)
+    @coordinates[xi][yi] = value
   end
 end
