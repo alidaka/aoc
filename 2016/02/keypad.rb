fe3918bbffa416faed31c4de8ec7e8f8f9142ca9
alidaka <alidaka@gmail.com>
2016-12-22 01:26:56 -0500

    2016-02 part 2

--- 2016/02/keypad.rb
+++ 2016/02/keypad.rb
@@ -1,7 +1,17 @@
 class Keypad
-  def self.parse(s)
-    layout = [['1','2','3'], ['4','5','6'], ['7','8','9']]
-    keypad = Keypad.new(layout, 1, 1)
+  def self.parse(s, format = :num)
+    keypad = case format
+    when :num
+      layout = [['1','2','3'], ['4','5','6'], ['7','8','9']]
+      Keypad.new(layout, 1, 1)
+    when :alphanum
+      layout = [[nil, nil, '1', nil, nil],
+                [nil, '2', '3', '4', nil],
+                ['5', '6', '7', '8', '9'],
+                [nil, 'A', 'B', 'C', nil],
+                [nil, nil, 'D', nil, nil]]
+      Keypad.new(layout, 2, 0)
+    end
 
     lines = s.split("\n")
     lines.each_with_object('') do |line, a|
