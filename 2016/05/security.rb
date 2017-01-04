b16685a277c9b302a03d54c3b79570ce6f8ea0a3
alidaka <alidaka@gmail.com>
2017-01-03 23:43:18 -0800

    2016-05 part 2; refactor part 1 to use yield

--- 2016/05/security.rb
+++ 2016/05/security.rb
@@ -2,16 +2,31 @@ require 'digest/md5'
 
 class Security
   def self.decode(seed)
-    suffix = 0
-    8.times.map do |x|
-      md5 = nil
-      loop do
-        md5 = Digest::MD5.hexdigest(seed + suffix.to_s)
-        suffix += 1
-        break if md5.start_with?('00000')
-      end
+    password = ''
+    md5_prefixes(seed) do |md5|
+      password += md5.chars[5]
+      return password if password.length == 8
+    end
+  end
 
-      md5.chars[5]
-    end.join
+  def self.decode_positional(seed)
+    password = Array.new(8)
+    md5_prefixes(seed) do |md5|
+      position = md5.chars[5].ord - 48
+      next if position < 0 || position > 7 || !password[position].nil?
+
+      password[position] = md5.chars[6]
+      return password.join if !password.include?(nil)
+    end
+  end
+
+  def self.md5_prefixes(seed)
+    suffix = 0
+    loop do
+      md5 = Digest::MD5.hexdigest(seed + suffix.to_s)
+      suffix += 1
+      puts suffix if md5.start_with?('00000')
+      yield md5 if md5.start_with?('00000')
+    end
   end
 end
--- 2016/05/spec.rb
+++ 2016/05/spec.rb
@@ -16,5 +16,16 @@ RSpec.describe Security do
   end
 
   describe 'part two' do
+    it 'does the example' do
+      data = 'abc'
+      password = Security.decode_positional(data)
+      expect(password).to eq '05ace8e3'
+    end
+
+    it 'solves the problem' do
+      data = 'uqwqemis'
+      password = Security.decode_positional(data)
+      expect(password).to eq '694190cd'
+    end
   end
 end
