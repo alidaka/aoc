de2cdac885733a271c170dac6be037e7de87c7e0
alidaka <alidaka@gmail.com>
2017-01-05 14:05:04 -0500

    2016-07 part 1

--- 2016/07/protocol.rb
+++ 2016/07/protocol.rb
@@ -1,5 +1,48 @@
 class Protocol
+  def self.count_supported_tls(s)
+    s.split("\n")
+      .count {|address| supports_tls?(address)}
+  end
+
+  def self.supports_ssl?(address)
+    supernets, hypernets = parse(address)
+    abas = supernets.flat_map { |a| find_abas(a) }
+    babs = hypernets.flat_map { |a| find_abas(a) }
+  end
+
+  def self.parse(address)
+    chunks = address.split('[')
+      .flat_map {|a| a.split(']')}
+
+    chunks.each_with_index
+      .partition {|x| (x[1]%2)==0}
+      .map {|a| a.map(&:first)}
+  end
+
   def self.supports_tls?(address)
+    supernets, hypernets = parse(address)
+    supernets.any? {|a| abba?(a)} && hypernets.none? {|a| abba?(a)}
+  end
+
+  def self.abba?(s)
+    s.chars.each_cons(4) do |chars|
+      return true if
+        chars[0] == chars[3] &&
+        chars[1] == chars[2] &&
+        chars[0] != chars[1]
+    end
+
     false
+  end
+
+  def self.find_abas(s)
+    acc = []
+    s.chars.each_cons(3) do |chars|
+      if chars[0] == chars[2] && chars[0] != chars[1]
+        acc << chars
+      end
+    end
+
+    acc
   end
 end
--- 2016/07/spec.rb
+++ 2016/07/spec.rb
@@ -1,14 +1,14 @@
 require_relative 'protocol'
 
 RSpec.describe Protocol do
-  describe 'part one' do
-    it 'requires the outer abba' do
+  describe 'part one: TLS, abba' do
+    it 'requires the supernet abba' do
       address = 'abba[mnop]qrst'
       result = Protocol.supports_tls?(address)
       expect(result).to be true
     end
 
-    it 'prohibits the inner abba' do
+    it 'prohibits the hypernet abba' do
       address = 'abcd[bddb]xyyx'
       result = Protocol.supports_tls?(address)
       expect(result).to be false
@@ -24,6 +24,56 @@ RSpec.describe Protocol do
       address = 'ioxxoj[asdfgh]zxcvbn'
       result = Protocol.supports_tls?(address)
       expect(result).to be true
+    end
+
+    it 'handles multiple hypernet sections' do
+      address = 'abcd[bdb]xyyx[foo]aaaa'
+      result = Protocol.supports_tls?(address)
+      expect(result).to be true
+    end
+
+    it 'prohibits an abba in a later hypernet section' do
+      address = 'abcd[fdfd]abcd[bddb]xyyx'
+      result = Protocol.supports_tls?(address)
+      expect(result).to be false
+    end
+
+    it 'solves the problem' do
+      addresses = File.read('input')
+      result = Protocol.count_supported_tls(addresses)
+      expect(result).to eq 105
+    end
+  end
+
+  xdescribe 'part two: SSL, aba/bab' do
+    it 'handles a simple positive case' do
+      address = 'aba[bab]xyz'
+      result = protocol.supports_ssl?(address)
+      expect(result).to be true
+    end
+
+    it 'handles a simple negative case' do
+      address = 'xyx[xyx]xyx'
+      result = protocol.supports_ssl?(address)
+      expect(result).to be false
+    end
+
+    it 'handles a positive case with aba at the end' do
+      address = 'aaa[kek]eke'
+      result = protocol.supports_ssl?(address)
+      expect(result).to be true
+    end
+
+    it 'handles a positive case with red herrings' do
+      address = 'zazbz[bzb]cdb'
+      result = protocol.supports_ssl?(address)
+      expect(result).to be true
+    end
+
+    it 'solves the problem' do
+      addresses = File.read('input')
+      result = Protocol.count_supported_ssl(addresses)
+      expect(result).to eq 0
     end
   end
 end
