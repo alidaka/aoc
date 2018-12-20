851c6871a7aa2f5b4fa133014f3dd1751ca8d42d
alidaka <alidaka@gmail.com>
2018-12-19 21:56:20 -0800

    2018 01-1

--- 2018/01.rb
+++ 2018/01.rb
@@ -1,3 +1,23 @@
 #!/usr/bin/env ruby
 
-puts 'hi'
+def calibrate(string)
+  string.split(/(, )|\n/)
+    .map(&:to_i)
+    .reduce(0, &:+)
+end
+
+if __FILE__ == $0
+  if ARGV.empty?
+    require 'minitest/autorun'
+
+    class CalibrateTest < Minitest::Test
+      def test_part_one
+        assert_equal(3, calibrate('+1, +1, +1'))
+        assert_equal(0, calibrate('+1, +1, -2'))
+        assert_equal(-6, calibrate('-1, -2, -3'))
+      end
+    end
+  else
+    p calibrate *ARGV
+  end
+end
