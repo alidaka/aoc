4d0cb744bf7d4ec31e30cca1a4b21f017b0f9e90
alidaka <alidaka@gmail.com>
2018-12-24 12:24:57 -0800

    2018 01-2

--- 2018/01.rb
+++ 2018/01.rb
@@ -1,9 +1,21 @@
 #!/usr/bin/env ruby
 
 def calibrate(string)
-  string.split(/(, )|\n/)
+  string.split(/,|\n/)
     .map(&:to_i)
     .reduce(0, &:+)
+end
+
+def calibrate2(string)
+  seen = []
+  string.split(/,|\n/)
+    .map(&:to_i)
+    .cycle
+    .each_with_object([0]) do |x, seen|
+      nxt = seen.last + x
+      return nxt if seen.include?(nxt)
+      seen << nxt
+    end
 end
 
 if __FILE__ == $0
@@ -16,8 +28,15 @@ if __FILE__ == $0
         assert_equal(0, calibrate('+1, +1, -2'))
         assert_equal(-6, calibrate('-1, -2, -3'))
       end
+
+      def test_part_two
+        assert_equal(0, calibrate2('+1, -1'))
+        assert_equal(10, calibrate2('+3, +3, +4, -2, -4'))
+        assert_equal(5, calibrate2('-6, +3, +8, +5, -6'))
+        assert_equal(14, calibrate2('+7, +7, -2, -7, -4'))
+      end
     end
   else
-    p calibrate *ARGV
+    p calibrate2 *ARGV
   end
 end
