ebb1cfe8c43962d5c69aa876ce16ab384f7ed209
alidaka <alidaka@gmail.com>
2018-12-25 06:46:45 +0000

    2018 03-2

--- 2018/03.rb
+++ 2018/03.rb
@@ -17,12 +17,37 @@ def overlap(string)
     end.reduce(&:+)
 end
 
+def overlap2(string)
+  n = 2000
+
+  seen_claims = Array.new
+
+  string.each_line
+    .map(&Claim.method(:new))
+    .each_with_object(Array.new(n) { Array.new(n, 0) }) do |claim, fabric|
+      (claim.y...claim.y + claim.height).each_entry do |y|
+        (claim.x...claim.x + claim.width).each_entry do |x|
+          previous_id = fabric[y][x]
+          if previous_id != 0 then
+            seen_claims[previous_id] = true
+            seen_claims[claim.id] = true
+          else
+            seen_claims[claim.id] |= false
+          end
+          fabric[y][x] = claim.id
+        end
+      end
+    end
+
+  seen_claims.find_index(false)
+end
+
 class Claim
   attr_reader :id, :x, :y, :width, :height
 
   def initialize(string)
     id, x, coordinates, size = string.split
-    @id = id.delete_prefix('#')
+    @id = id.delete_prefix('#').to_i
     @x, @y = coordinates.split(',').map(&:to_i)
     @width, @height = size.split('x').map(&:to_i)
   end
@@ -43,9 +68,15 @@ if __FILE__ == $0
       end
 
       def test_part_two
+        input = <<~HEREDOC
+          #1 @ 1,3: 4x4
+          #2 @ 3,1: 4x4
+          #3 @ 5,5: 2x2
+        HEREDOC
+        assert_equal(3, overlap2(input))
       end
     end
   else
-    p overlap *ARGV
+    p overlap2 *ARGV
   end
 end
